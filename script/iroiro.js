22851962aec50d78fa0147a579df39656d9f404e
makkimakky <m011530928@edu.teu.ac.jp>
2017-07-23 21:08:05 +0900

    7/23

---
+++ script/iroiro.js
@@ -0,0 +1,17 @@
+
+//contentフェードイン処理
+
+$(function(){
+    $('#content').hide();
+    $('#content').fadeIn(1500);
+});
+
+
+
+// $(document).ready(function(){
+//
+// $(function(){
+//   $('#title_rogo').slideDown('slow');
+// });
+//
+// });
